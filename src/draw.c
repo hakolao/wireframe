56a93998edf0149f8b654912236b65898fb1c1b6
Okko Hakola <ohakola@c1r5p14.hive.fi>
2020-01-21 13:39:07 +0200

    Fix one mem leak

--- src/draw.c
+++ src/draw.c
@@ -6,7 +6,7 @@
 /*   By: ohakola <ohakola@student.helsinki.fi>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/11 13:03:22 by ohakola           #+#    #+#             */
-/*   Updated: 2020/01/21 13:09:47 by ohakola          ###   ########.fr       */
+/*   Updated: 2020/01/21 13:38:42 by ohakola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,25 +82,32 @@ static void			draw_map(t_scene *scene)
 
 static void			vector_to_ui(t_scene *scene, t_vector *v, int xpos, int ypos)
 {
-	char x[50];
-	char y[50];
-	char z[50];
-	char *vectorstr;
+	char	*vectorstr;
+	char	*x;
+	char	*y;
+	char	*z;
 
-	ft_strcpy(x, ft_itoa(v->v[0]));
-	ft_strcpy(y, ft_itoa(v->v[1]));
-	ft_strcpy(z, ft_itoa(v->v[2]));
-	if ((vectorstr = ft_strjoin(x, ", ")) == NULL ||
-		(vectorstr = ft_strjoin(vectorstr, y)) == NULL ||
-		(vectorstr = ft_strjoin(vectorstr, ", ")) == NULL ||
-		(vectorstr = ft_strjoin(vectorstr, z)) == NULL)
+	if ((x = ft_itoa(v->v[0])) == NULL ||
+		(y = ft_itoa(v->v[1])) == NULL ||
+		(z = ft_itoa(v->v[2])) == NULL)
+		return ;
+	if ((vectorstr = ft_strnew(256)) == NULL ||
+		ft_strcpy(vectorstr, x) == NULL ||
+		ft_strcat(vectorstr, ", ") == NULL ||
+		ft_strcat(vectorstr, y) == NULL ||
+		ft_strcat(vectorstr, ", ") == NULL ||
+		ft_strcat(vectorstr, z) == NULL)
 		return ;
 	mlx_string_put(scene->mlx, scene->mlx_wdw, xpos, ypos, UI_COLOR, vectorstr);
+	ft_strdel(&vectorstr);
+	ft_strdel(&x);
+	ft_strdel(&y);
+	ft_strdel(&z);
 }
 
 static void			draw_ui(t_scene *scene)
 {
-	mlx_string_put(scene->mlx, scene->mlx_wdw, 10, 20, UI_COLOR, "Camera position:");
+	mlx_string_put(scene->mlx, scene->mlx_wdw, 10, 20, UI_COLOR, "Camera xyz:");
 	vector_to_ui(scene, scene->camera->position, 10, 35);
 }
 
