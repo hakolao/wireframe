04f4d07bab762ac86582501b526f8c789e85f083
Okko Hakola <ohakola@c2r6p4.hive.fi>
2020-01-29 17:10:37 +0200

    Optimize vector list multiplication

--- libmatrix/ft_matrix_mul_vector_lst.c
+++ libmatrix/ft_matrix_mul_vector_lst.c
@@ -6,7 +6,7 @@
 /*   By: ohakola <ohakola@student.helsinki.fi>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/21 14:24:25 by ohakola           #+#    #+#             */
-/*   Updated: 2020/01/21 14:25:43 by ohakola          ###   ########.fr       */
+/*   Updated: 2020/01/29 17:10:01 by ohakola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,18 @@
 int		ft_matrix_mul_vector_lst(t_matrix *m, t_vector **vecs, size_t n)
 {
 	size_t		i;
-	t_vector	*vec;
+	t_vector	vec;
 
 	i = 0;
 	while (i < n)
 	{
-		if ((vec = ft_vector4_new(0, 0, 0)) == NULL ||
-			ft_matrix_mul_vector(m, vecs[i], vec) == FALSE)
+		vec = (t_vector){.v = (double[4]){0, 0, 0, 0}, .size = 4};
+		if (ft_matrix_mul_vector(m, vecs[i], &vec) == FALSE)
 			return (0);
-		vec->v[3] = 1;
-		ft_vector_free(vecs[i]);
-		vecs[i] = vec;
+		vecs[i]->v[0] = vec.v[0];
+		vecs[i]->v[1] = vec.v[1];
+		vecs[i]->v[2] = vec.v[2];
+		vecs[i]->v[3] = 1;
 		i++;
 	}
 	return (1);
